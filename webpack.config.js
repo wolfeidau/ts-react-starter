9c6775a9921fb8f6af23adcbb07a262a5d337e99
Mark Wolfe <mark@wolfe.id.au>
2016-07-11 09:37:15 +1000

    Fixed missing webpack file.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const NODE_ENV = process.env.NODE_ENV;
+const dotenv = require('dotenv');
+
+const getConfig = require('hjs-webpack');
+
+const isDev  = NODE_ENV === 'development';
+const isTest = NODE_ENV === 'test';
+
+
+var config = getConfig({
+  isDev: isDev,
+  in: 'src/index.tsx',
+  out: 'public',
+  html: function (context) {
+    return {
+      'index.html': context.defaultTemplate({
+        title: 'auth0 React Sample',
+        publicPath: isDev ? 'http://localhost:3000/' : '',
+        meta: {
+          'name': 'AWS Toolchest',
+          'description': 'A chest filled with various AWS tools.'
+        }
+      })
+    }
+  }
+});
+
+// workaround see https://github.com/HenrikJoreteg/hjs-webpack/issues/227
+var tsLoaderConfig = config.module.loaders.find(loaderConfig =>
+  loaderConfig.loaders && loaderConfig.loaders[0] === 'awesome-typescript-loader'
+);
+
+tsLoaderConfig.loaders = [
+    'react-hot',
+    'awesome-typescript-loader'
+  ];
+
+config.resolve.extensions = config.resolve.extensions.concat(['.ts', '.tsx']);
+// end workaround
+
+module.exports = config;
